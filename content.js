e2afc12ed586792e456ebe7e812c703993a62b28
ukextreme <udaykeshav2005@gmail.com>
2025-06-02 00:24:16 +0530

    Added the Bookmark icon on the AZ problem page

--- content.js
+++ content.js
@@ -1,7 +1,15 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+
+const bookmarkImgURL = chrome.runtime.getURL("assets/bookmark.png");
+
+window.addEventListener("load", addBookmarkButton);
+
+function addBookmarkButton(){
+    const bookmarkButton = document.createElement('img');
+    bookmarkButton.id = "add-bookmark-button";
+    bookmarkButton.src = bookmarkImgURL;
+    bookmarkButton.style.height = "30px";
+    bookmarkButton.style.width = "30px";
+    const topicname = document.getElementsByClassName("coding_problem_info_heading__G9ueL")[0];
+    topicname.insertAdjacentElement("beforebegin",bookmarkButton);
+}
+
